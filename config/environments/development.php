5ef6fba52bab7e8340977acb9c46d14b4210dc60
mAAdhaTTah <jamesorodig@gmail.com>
2014-11-12 04:33:46 +0100

    Initial commit

---
+++ config/environments/development.php
@@ -0,0 +1,15 @@
+<?php
+/* Development */
+define('DB_NAME', getenv('DB_NAME'));
+define('DB_USER', getenv('DB_USER'));
+define('DB_PASSWORD', getenv('DB_PASSWORD'));
+define('DB_HOST', getenv('DB_HOST') ? getenv('DB_HOST') : 'localhost');
+
+define('WP_HOME', getenv('WP_HOME'));
+define('WP_SITEURL', getenv('WP_SITEURL'));
+
+define('SAVEQUERIES', true);
+define('WP_DEBUG', true);
+define('WP_DEBUG_DISPLAY', false);
+define('WP_DEBUG_LOG', true);
+define('SCRIPT_DEBUG', true);
