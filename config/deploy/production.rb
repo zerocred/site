3a87d325c6431a4f6d6d428f93f073fd511f94d3
mAAdhaTTah <jamesorodig@gmail.com>
2014-12-15 03:59:03 +0100

    Fix IP address & deploy key

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -8,14 +8,14 @@ set :stage, :production
 
 # Extended Server Syntax
 # ======================
-server 'zerocred.club', user: 'deploy', roles: %w{web app db}
+server '104.236.111.107', user: 'deploy', roles: %w{web app db}
 
 # you can set custom ssh options
 # it's possible to pass any option but you need to keep in mind that net/ssh understand limited list of options
 # you can see them in [net/ssh documentation](http://net-ssh.github.io/net-ssh/classes/Net/SSH.html#method-c-start)
 # set it globally
  set :ssh_options, {
-   keys: %w(~/.ssh/zerocred-deploy),
+   keys: %w(~/.ssh/digitalocean-deploy),
    forward_agent: true,
    auth_methods: %w(publickey)
  }
